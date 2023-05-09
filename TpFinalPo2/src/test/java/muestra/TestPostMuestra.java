2f833a72354d5751045595d28df5110536a51f3e
Mauro-asd <85760409+Mauro-asd@users.noreply.github.com>
2023-05-09 14:44:28 -0300

    Add files via upload

---
+++ TpFinalPo2/src/test/java/muestra/TestPostMuestra.java
@@ -0,0 +1,15 @@
+package muestra;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+public class TestPostMuestra {
+	
+	@Test
+	public void test() {
+		mock(PostMuestra.class);
+		assertTrue(true);
+	}
+}
