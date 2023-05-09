e0e55f7a059e3a89ba22da7087e3ce3353a6f7ef
roman <roman@LAPTOP-S8A5CM26>
2023-05-09 15:12:30 -0300

    Probando

--- TpFinalPo2/src/test/java/muestra/TestVerificadorMuestra.java
+++ TpFinalPo2/src/test/java/muestra/TestVerificadorMuestra.java
@@ -1,18 +1,16 @@
 package muestra;
 
-
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 
+public class TestVerificadorMuestra {
 
-
-public class TestPostMuestra {
-	
 	@Test
 	public void test() {
-		mock(PostMuestra.class);
+		mock(VerificadorMuestra.class);
 		assertTrue(true);
+		
 	}
 }
