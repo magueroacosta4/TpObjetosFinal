2a472bee8a032502c65838d10404904efee3e49e
afernandodan <afernandodan@gmail.com>
2023-05-09 23:42:51 -0300

    holaaaaaaaaaaa

--- TpFinalPo2/src/test/java/organizacion/OrganizacionTest.java
+++ TpFinalPo2/src/test/java/organizacion/OrganizacionTest.java
@@ -11,31 +11,8 @@ import muestra.*;
 public class OrganizacionTest {
 	private TipoOrganizacion unTipoOrga;
 	private Ubicacion unaUbicacion;
-	private Organizacion unaOrga, otraOrga, orgaEdu, orgaCult, orgaAsist;
-	private PaginaWeb unaPagina;
-
-	@Before
-	public void setUp() {
-		unTipoOrga = TipoOrganizacion.SALUD;
-		unaUbicacion = mock(Ubicacion.class);
-		unaPagina = mock(PaginaWeb.class);
-		unaOrga = new Organizacion(unaUbicacion, unTipoOrga, 10, unaPagina);
-		otraOrga = new Organizacion(unaUbicacion, unTipoOrga, 25, unaPagina);
-		
-		orgaEdu = new Organizacion(unaUbicacion, TipoOrganizacion.EDUCATIVA, 10, unaPagina);
-		orgaCult = new Organizacion(unaUbicacion, TipoOrganizacion.CULTURAL, 10, unaPagina);
-		orgaAsist = new Organizacion(unaUbicacion, TipoOrganizacion.ASISTENCIA, 10, unaPagina);
-	}
-	
-	@Test
-	public void unaOrganizacionTieneUbicacionTipoYCantidadDePersonasQueTrabajanEnEllaYUnaPagina() {
-		assertEquals(unaOrga.getUbicacion(), unaUbicacion);
-		assertEquals(unaOrga.getTipo(), unTipoOrga);
-		assertEquals(unaOrga.getCantidadTrabajadores(), 10);
-		assertEquals(unaOrga.getPaginaWeb(), unaPagina);
-	}
-	
-	@Test
+	privatunTipoOrga = TipoOrganizacion.SALUD;
+		un
 	public void otraOrganizacionTieneUnaCantidadDiferenteDeTrabajadores() {
 		assertEquals(otraOrga.getCantidadTrabajadores(), 25);
 	}
