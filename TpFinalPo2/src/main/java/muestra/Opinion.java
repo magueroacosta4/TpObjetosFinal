850523c7436a7a6cb4d7b3dd62f913d56bbdd813
roman <roman@LAPTOP-S8A5CM26>
2023-05-09 22:34:46 -0300

    Sigo probando

---
+++ TpFinalPo2/src/main/java/muestra/Opinion.java
@@ -0,0 +1,14 @@
+package muestra;
+
+import java.util.List;
+
+public enum Opinion {
+	
+	;
+
+	public static List<Opinion> getOpiniones() {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ TpFinalPo2/src/main/java/muestra/Ubicacion.java
@@ -0,0 +1,5 @@
+package muestra;
+
+public class Ubicacion {
+
+}
