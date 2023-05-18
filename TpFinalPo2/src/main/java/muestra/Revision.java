03c1aa863a9909640805f089bb3fb4053c76088f
Mauro-asd <85760409+Mauro-asd@users.noreply.github.com>
2023-05-18 17:04:55 -0300

    modifique revision
    agregue el colaborador user

--- TpFinalPo2/src/main/java/muestra/Revision.java
+++ TpFinalPo2/src/main/java/muestra/Revision.java
@@ -3,17 +3,29 @@ package muestra;
 import java.time.LocalDate;
 
 import Usuario.EstadoUsuario;
+import Usuario.Usuario;
 
 public class Revision {
 
 	private Opinion opinion;
 	private LocalDate getFechaDeCreacion;
 	private EstadoUsuario estadoDelUsuarioActual;
+	private Usuario user;
 	
-	public Revision(Opinion o, EstadoUsuario e) {
+	public Revision(Opinion o, EstadoUsuario e, Usuario u) {
 		setFechaDeCreacion();
+		setUsuario(u);
 		setEstadoDelUsuarioActual(e);
 		setOpinion(o);
+	}
+
+	private void setUsuario(Usuario u) {
+		// TODO Auto-generated method stub
+		this.user = u;
+	}
+	
+	public Usuario getUser() {
+		return user;
 	}
 
 	public Opinion getOpinion() {
